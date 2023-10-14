07495133a75b1e04130df9463ac538c1afe114b9
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-10-14 22:05:20 +0200

    add comments

--- arg_parser.cpp
+++ arg_parser.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unistd.h>
-#include <regex>
+// ISA 2023/2024
+// Author : Jiří Štípek (xstipe02)
+// Description: Agrument parser
+
 #include "arg_parser.h"
 
 // func to valid correct format of ip adress
 bool is_IPv4_valid(std::vector<std::string> &addresses, char *ip_address)
 {
     // Define a regular expression pattern for IPv4 with CIDR notation
-    std::regex ipv4WithCIDRPattern("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}/\\d{1,2}$");
+    std::regex IPv4_with_CIDR_pattern("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}/\\d{1,2}$"); //CIDR notation "0.0.0.0/24"
 
     // Check each IP address in the vector
-    if (!std::regex_match(std::string(ip_address), ipv4WithCIDRPattern))
+    if (!std::regex_match(std::string(ip_address), IPv4_with_CIDR_pattern))
     {
         return false; // If any IP is invalid, return false
     }
@@ -21,7 +20,7 @@ bool is_IPv4_valid(std::vector<std::string> &addresses, char *ip_address)
     return true; // All IPs are valid
 }
 
-// main function to parse
+// main parse function
 struct arguments arg_parse(int argc, char *const *argv)
 {
     std::vector<std::string> empty_vector{"NULL"};
@@ -32,25 +31,21 @@ struct arguments arg_parse(int argc, char *const *argv)
         switch (opt)
         {
         case 'r':
-
             args.filename = optarg;
             break;
         case 'i':
-
             args.interface = optarg;
             break;
         case '?':
             print_help();
-            exit(EXIT_FAILURE);
+            exit_program("Unknown option.");
         }
     }
-
     for (int i = optind; i < argc; i++)
     {
-        if (!is_IPv4_valid(args.ipPrefixes, argv[i]))
+        if (!is_IPv4_valid(args.IP_prefixes, argv[i]))
         {
-            std::cout << "Some or all IP addresses are not valid." << std::endl; // invalid ip adress
-            exit(EXIT_FAILURE);
+            exit_program("Some or all IP addresses are not valid."); // invalid ip adress
         }
     }
 
@@ -75,20 +70,20 @@ void validate(struct arguments *args)
     if (args->filename == "NULL" && args->interface == "NULL")
     {
         print_help();
-        exit(EXIT_FAILURE);
+        exit_program("You must specify either a filename or an interface.");
     }
     if (args->filename != "NULL" && args->interface != "NULL")
     {
         print_help();
-        exit(EXIT_FAILURE);
+        exit_program("You must specify either a filename or an interface, not both.");
     }
-    if (args->ipPrefixes.size() == 1)
+    if (args->IP_prefixes.size() == 1)
     {
         print_help();
-        exit(EXIT_FAILURE);
+        exit_program("You must specify at least one IP prefix.");
     }
     else
     {
-        args->ipPrefixes.erase(args->ipPrefixes.begin());
+        args->IP_prefixes.erase(args->IP_prefixes.begin());
     }
 }
