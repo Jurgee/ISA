64ab185b38c7f93cb58f697a5e51d97f076b2d26
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-10-14 22:05:52 +0200

    add header comms

--- dhcp_stats.cpp
+++ dhcp_stats.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
-#include <unistd.h>
-#include <vector>
-#include "arg_parser.h"
+// ISA 2023/2024
+// Author : Jiří Štípek (xstipe02)
+// Description: Main file of the program
+
 #include "dhcp_monitor.h"
 #include "ncurses_logger.h"
 
 int main(int argc, char *argv[])
 {
-    initialize_ncurses();
-
     DHCP_monitor(argc, argv);
-
     cleanup_ncurses();
     return 0;
 }
