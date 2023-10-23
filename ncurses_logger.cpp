ad99e2ee4b4b836d3c68ccd564116767cf2d162d
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-10-23 11:14:04 +0200

    sigint func

--- ncurses_logger.cpp
+++ ncurses_logger.cpp
@@ -35,3 +35,23 @@ void exit_program(const std::string &message)
     fprintf(stderr, "%s \n", message.c_str());
     exit(1);
 }
+
+// signal handler for SIGINT
+void sigint_handler(int signum)
+{
+    (void)signum;
+    cleanup_ncurses();
+    closelog();
+    exit(0);
+}
+
+// print help
+void print_help()
+{
+    std::cerr << "./dhcp-stats [-r <filename>] [-i <interface-name>] <ip-prefix> [ <ip-prefix> [ ... ] ] \n \
+            -r <filename> - statistika bude vytvořena z pcap souborů \n \
+            -i <interface> - rozhraní, na kterém může program naslouchat \n \
+            <ip-prefix> - rozsah sítě pro které se bude generovat statistika "
+              << std::endl;
+
+}
--- ncurses_logger.h
+++ ncurses_logger.h
@@ -2,7 +2,11 @@
 #include <ncurses.h>
 #include <syslog.h>
 
+// functions
+
 void log_exceeded_prefix(const std::string &prefix);
 void initialize_ncurses();
 void cleanup_ncurses();
 void exit_program(const std::string &message);
+void sigint_handler(int signum);
+void print_help();
