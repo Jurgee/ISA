dc20a99d0869ead884c7cf03ceb6775943c918dc
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-11-15 17:01:03 +0100

    Revert "Delete .vscode directory"
    This reverts commit 80d72d6a448fece90374b4f67c7ac89b59c23ee8.

--- ncurses_logger.cpp
+++ ncurses_logger.cpp
@@ -4,12 +4,20 @@
 
 #include "ncurses_logger.h"
 
+// set for storing prefixes that exceeded 50% of allocated addresses
+std::set<std::string> exceeded_prefixes;
+
 // if we have more than 50% of IP addresses allocated, log it
 void log_exceeded_prefix(const std::string &prefix)
 {
-    // Log to syslog
-    syslog(LOG_NOTICE, "prefix %s exceeded 50%% of allocations.\n", prefix.c_str());
+    if (exceeded_prefixes.find(prefix) == exceeded_prefixes.end()) // is not in set
+    {
+        // Log to syslog
+        syslog(LOG_NOTICE, "prefix %s exceeded 50%% of allocations.\n", prefix.c_str());
 
+        // Add the prefix to the set of exceeded prefixes
+        exceeded_prefixes.insert(prefix);
+    }
     // Display in the ncurses window
     printw("prefix %s exceeded 50%% of allocations.\n", prefix.c_str());
     refresh();
@@ -22,6 +30,7 @@ void initialize_ncurses()
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
+    mvprintw(0, 0, "IP-Prefix Max-hosts Allocated addresses Utilization\n"); // Print the header
     refresh();
 }
 
@@ -31,14 +40,12 @@ void cleanup_ncurses()
     endwin();
 }
 
-// Exit the program with the given message
+// Exit the program with the given message to stderr
 void exit_program(const std::string &message)
 {
-    fprintf(stderr, "%s \n", message.c_str());
+    std::cerr << message << std::endl;
     exit(1);
 }
-
-
 
 // print help
 void print_help()
